76530ca575dd42c1beb78b37a8d7df5ea9f22523
Robin Liu <71776622+roobnloo@users.noreply.github.com>
2021-11-28 20:37:33 -0800

    general spatial plots for first 3 factors

--- src/spatialplot.py
+++ src/spatialplot.py
@@ -1,10 +1,10 @@
 import numpy as np
 import dask.dataframe as dd
 import pandas as pd
-import tensorly as tl
 import matplotlib.pyplot as plt
 import configparser
 import json
+import mercantile
 
 
 def dump_quadkey_map(parquet_path, path_to_quadkey_map):
@@ -18,7 +18,44 @@ def dump_quadkey_map(parquet_path, path_to_quadkey_map):
 
 def read_quadkey_map(path_to_quadkey_map):
     quadkey_map = json.load(open(f"{path_to_quadkey_map}quadkey_map.json"))
+    quadkey_map = {int(k): v for k, v in quadkey_map.items()}
     return quadkey_map
+
+
+def generate(coords, rank, fact_dir, figures_dir):
+    spatial_factors = np.load(fact_dir + f"rank_{str(rank)}_factor_0.npy")
+    multiidx = pd.MultiIndex.from_tuples(coords)
+    dominant = get_movement_matrix(spatial_factors, 0, multiidx)
+    subdominant = get_movement_matrix(spatial_factors, 1, multiidx)
+    thirddominant = get_movement_matrix(spatial_factors, 2, multiidx)
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6))
+    plt.suptitle(f"Rank {rank} decomposition")
+    ax1.pcolormesh(dominant)
+    ax1.set_title("Factor 1")
+    ax2.pcolormesh(subdominant)
+    ax2.set_title("Factor 2")
+    ax3.pcolormesh(thirddominant)
+    ax3.set_title("Factor 3")
+    plt.savefig(f"{figures_dir}spatialplot_rank{rank}")
+    plt.clf()
+
+
+def get_movement_matrix(spatial_factors, factor_idx, multi_idx):
+    mx = spatial_factors[:, factor_idx]
+    mx = pd.Series(mx, index=multi_idx).to_xarray().to_numpy()
+    return mx
+
+
+def get_coordinates(dir_parquet):
+    df = dd.read_parquet(dir_parquet)
+    sorted_geos = df.geography.unique().compute().sort_values()
+    coords = [get_coord(g) for g in sorted_geos]
+    return coords
+
+
+def get_coord(qk):
+    bounds = mercantile.bounds(mercantile.quadkey_to_tile(qk))
+    return (bounds.north + bounds.south)/2, (bounds.east + bounds.west)/2
 
 
 if __name__ == '__main__':
@@ -26,5 +63,7 @@ if __name__ == '__main__':
     config.read('config.ini')
     dir_to_save_factors = config['PATHS']['dir_to_save_factors']
     dir_save_figures = config['PATHS']['path_to_save_figures']
-    parquet_dir = config['PATHS']['path_to_scu_parquet']
-    dump_quadkey_map(parquet_dir, dir_save_figures)
+    dir_parquet = config['PATHS']['path_to_poi_agg_parquet']
+    coordinates = get_coordinates(dir_parquet)
+    for rank in range(3, 21):
+        generate(coordinates, rank, dir_to_save_factors, dir_save_figures)
